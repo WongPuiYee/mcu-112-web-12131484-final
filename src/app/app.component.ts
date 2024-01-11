b16c9800b5c0ff1b70c3b33807eb101e88ce3d9f
Puiyee <Pyeewong11@gmail.com>
2024-01-12 03:34:44 +0800

    feat(homepage)-首頁

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
-
+import { ArticleComponent } from './article/article.component';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [HeaderComponent],
+
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
